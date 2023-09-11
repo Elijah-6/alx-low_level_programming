4b1777c525344dbfa2b8c7b1161c1c718b12e811
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-09-11 14:20:08 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - print all combinations
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, j;
+	int a, b, c, d;
+
+	for (i = 0; i <= 99; i++)
+	{
+		a = i / 10 + '0';
+		b = i % 10 + '0';
+
+		for (j = 0; j <= 99; j++)
+		{
+			c = j / 10 + '0';
+			d = j % 10 + '0';
+
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a);
+				putchar(b);
+				putchar(' ');
+				putchar(c);
+				putchar(d);
+			
+				if (i != 9 && j != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
