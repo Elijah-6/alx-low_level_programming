d7d34be8009eaf9cd9dd173b7ce155c7aa95437b
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-09-12 07:22:52 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * _abs - returns the absolute value of a number
+ *
+ * @n: number to return it's abs
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
