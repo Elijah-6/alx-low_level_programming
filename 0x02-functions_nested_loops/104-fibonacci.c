4903656ab83e3affa9cf72090a82fe22145e5d86
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-09-13 08:33:20 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print the firs 98 fibo
+ *
+ * Return: return nothing
+ */
+
+int main(void)
+{
+	unsigned long a = 1, b = 2, temp;
+	int i;
+
+	printf("%lu, %lu, ", a, b);
+
+	for  (i = 3; i <= 98; i++)
+	{
+		temp = a + b;
+
+		printf("%lu, ", temp);
+
+		a = b;
+		b = temp;
+	}
+	printf("\n");
+	return (0);
+}
+
