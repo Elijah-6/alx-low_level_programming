f22a01812d58845526af2e43fabd9e2cf0991717
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-09-18 18:03:52 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_array - print the elements of an array
+ *
+ * @a: array
+ *
+ * @n: number of elements to print from array
+ */
+
+void print_array(int *a, int n)
+{
+	int c;
+
+	for (c = 0; c <= n-1; c++)
+	{
+		printf("%d, ", a[c]);
+	}
+	putchar('\n');
+}
