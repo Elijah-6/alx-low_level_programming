79542787d441fc1bf5770275a9f413f0ed290a80
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-09-22 17:35:24 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,6 +3,7 @@
  * _strncat - concatenates two strings,
  * @dest: destination.
  * @src: source.
+ * @n: number of chars to add.
  * Return: the pointer to dest.
  */
 char *_strncat(char *dest, char *src, int n)
