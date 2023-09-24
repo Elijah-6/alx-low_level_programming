f427460d45711423b616b085dcc07c91fc146330
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-09-23 17:08:01 -0700

    added _putchar

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,3 +8,6 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+char *_strncpy(char *dest, char *src, int n);
+char *leet(char *);
+int _putchar(char c);
