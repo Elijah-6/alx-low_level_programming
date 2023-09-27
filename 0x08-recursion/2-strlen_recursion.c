431490ed84524d4dd5a176ee5d91910d8d9ed6bb
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-09-27 08:41:30 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+
+/**
+ * _strlen_recursion - print the length of a string.
+ *
+ * @s : String to be counted
+ *
+ * Return : void
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
