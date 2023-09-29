c2c5a6683f657db317c3adf955c3ffd24e26e9a7
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-09-29 07:11:42 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,10 @@
 /**
  * main - print program name
  *
- * @argc - number of arguments
- * @argv - array of arguments
- * 
- * Return - Always 0
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
