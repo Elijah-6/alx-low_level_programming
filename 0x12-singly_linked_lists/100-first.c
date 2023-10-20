9b73de251366df900b3764c28fe114ae59480d70
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-10-19 17:11:16 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 void my_constructor(void) __attribute__((constructor));
-
+/**
+ * my_constructor - function with constructor
+ *
+ * Description: print string`
+ */
 void my_constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
