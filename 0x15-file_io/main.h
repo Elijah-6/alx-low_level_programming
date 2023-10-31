974831dcf18e799b9883c1838e899cbd9e682276
Elijah Frempong <elijahfrimpong76@gmail.com>
2023-10-31 13:51:37 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define BUFSIZE 1204
 
 /* Function prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
